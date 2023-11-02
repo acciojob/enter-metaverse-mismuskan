783ac864b371e761ad025fd9e4095dfe2971de9e
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2023-11-02 18:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 //your JS code here. If required.
 var p=document.getElementById("status");
 var but=document.getElementById("enterBtn");
+var out=document.getElementById("out");
 enterBtn.addEventListener("click",function(){
-	// p.style.color="red";
-	// p.textContent=window.object.h1"headingText";
 	p.textContent="Entered Metaverse";
-})
+	// out.textContentp.textContent;
+	 out.textContent = ''; // Clear the text inside the <h1> element
+    var h1Element = document.createElement("h1");
+    h1Element.textContent = p.textContent;
+    out.appendChild(h1Element);
+});
