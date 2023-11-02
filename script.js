71a8078c64808ba3b99462906f69c11364311fe6
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2023-11-02 18:36:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ var p=document.getElementById("status");
 var but=document.getElementById("enterBtn");
 var out=document.getElementById("out");
 enterBtn.addEventListener("click",function(){
-	p.textContent="Entered Metaverse";
-	// out.textContentp.textContent;
-	 out.textContent = ''; // Clear the text inside the <h1> element
-    var h1Element = document.createElement("h1");
-    h1Element.textContent = p.textContent;
+	 // out.textContent = ''; // Clear the text inside the <h1> element
+    // var h1Element = document.createElement("h1");
+    // h1Element.textContent = p.textContent;
+    // out.appendChild(h1Element);
+	p.remove();
+	 var h1Element = document.createElement("h1");
+    h1Element.textContent = "Entered Metaverse";
+    out.textContent = ''; // Clear any existing content in the "out" element
     out.appendChild(h1Element);
 });
