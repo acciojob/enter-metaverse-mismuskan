79aa30036ab567c5ce6431128411760bc949129a
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2023-11-02 10:55:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+var p=document.getElementById("status");
+var but=document.getElementById("enterBtn");
+enterBtn.addEventListener("click",function(){
+	p.textContent="Entered Metaverse";
+})
